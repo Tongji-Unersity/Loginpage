788e7dc63e028eb76698b0e888be1a5cf7b47a34
Angeldoo <87635769+Angeldoo@users.noreply.github.com>
2022-07-31 01:06:08 +0800

    Add files via upload

---
+++ logincheck.js
@@ -0,0 +1,33 @@
+
+jQuery(document).ready(function() {
+
+    $('.loginpage form').submit(function(){
+        var username = $(this).find('.username').val();
+        var password = $(this).find('.password').val();
+        if(username == '') {
+            $(this).find('.error').fadeOut('fast', function(){
+                $(this).css('top', '27px');
+                alert("用户名不正确！");
+            });
+            $(this).find('.error').fadeIn('fast', function(){
+                $(this).parent().find('.username').focus();
+            });
+            return false;
+        }
+		
+        if(password == '') {
+            $(this).find('.error').fadeOut('fast', function(){
+                $(this).css('top', '96px');
+            });
+            $(this).find('.error').fadeIn('fast', function(){
+                $(this).parent().find('.password').focus();
+            });
+            return false;
+        }
+    });
+
+    $('.loginpage form .username, .loginpage form .password').keyup(function(){
+        $(this).parent().find('.error').fadeOut('fast');
+    });
+
+});
